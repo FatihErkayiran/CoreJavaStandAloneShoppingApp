92a6232c01f6800c29475b2b2af5b4212be18973
FatihErkayiran <fatiherkayiran@gmail.com>
2020-10-02 16:37:03 -0500

    view class is created

---
+++ src/com/ecommerce/application/View.java
@@ -0,0 +1,175 @@
+package com.ecommerce.application;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.ecommerce.controller.Controller;
+import com.ecommerce.model.Customer;
+import com.ecommerce.model.Item;
+
+public class View {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		//Controller controller =new Controller();
+		int count=0;
+		int counter=1;
+		File file =new File("resources/"+ counter + ".data");
+		List<Item>items=new ArrayList<>();
+		boolean state=true;
+	while(state) {
+		try {
+			
+			System.out.println("ECommerce App Welcomes You ");
+			Scanner scanner =new Scanner(System.in);
+			System.out.println("1.Register\n2.Login");
+			int choice=scanner.nextInt();
+			scanner.nextLine();
+				if (choice==1) {
+					boolean case1=true;
+				//  while(case1)
+					System.out.println("Enter Details for the Account");
+					System.out.println("User Name");
+					String userName=scanner.nextLine();
+					System.out.println("Password");
+					String password=scanner.nextLine();
+	                System.out.println("Confirm Password");
+	                String conPassword=scanner.nextLine();
+	//                if (password.equals(conPassword)) {
+	//                	System.out.println("Passwords do not match");
+	//                	case1=false;
+	//				     }
+	                try {
+	    				file.createNewFile();
+	    			} catch (IOException e) {
+	    				// TODO Auto-generated catch block
+	    				e.printStackTrace();
+	    			}
+	                
+				Customer customer=new Customer(userName, conPassword);
+			    
+				Controller.writeObjToFile(file, customer);
+	//			controller.createAccount(customer);
+				counter++;
+				}
+			
+				if (choice==2) {
+				System.out.println(" Enter Login Details");
+				System.out.println(" User ID");
+				String userName=scanner.nextLine();
+				System.out.println(" Password ");
+				String password=scanner.nextLine();
+				Customer cust=Controller.readObjFromFile(file);
+				
+//			    Customer cust= controller.loginCustomer(userName, password);
+				 
+				 if (cust.getUserName().equals(userName) && cust.getPassword().equals(password)) {
+						System.out.println(" Welcome " + cust.getUserName());
+						System.out.println("3.Buy An Item\n4.Replace An Item\n5.Exit");
+						int choiceAgain=scanner.nextInt();
+					if (choiceAgain==3) {
+						boolean state2=true;
+						while(state2) {
+						System.out.println("Items  Items Code   Price");
+						System.out.println("1.Jacket Ja1  20$\n2.Jeans Je1  10$\n3.Shirt Se1 5$\n4.Check Out\n5.Sign Out");
+						int itemChoice=scanner.nextInt();
+						scanner.nextLine();
+						 if (itemChoice==1) {
+							Item jacket=new Item("Jacket", "Ja1", "20$");
+							items.add(jacket);
+						  }
+						 else if(itemChoice==2) {
+							 Item jeans=new Item("Jeans", "Je1", "10$");
+							 items.add(jeans);
+						  }
+						 else if(itemChoice==3) {
+							 Item shirt=new Item("Shirt", "Se1", "5$");
+							 items.add(shirt);
+						 }
+						 else if(itemChoice==4) {
+							 Integer nInteger=(int) ((Math.random())/12);
+							 System.out.println("Customer Name " + cust.getUserName() + " "+LocalDateTime.now()+
+									 "\nInvoice Number: " + nInteger);
+//							 System.out.println(items.size());
+//							 for (Item item : items) {
+//								System.out.println(item + "\n");
+//								
+//								
+//							}
+							
+							 
+							 for (int i = 0; i < items.size(); i++) {
+								 System.out.println(items.get(i) + "\n");
+								 String newPrice=items.get(i).getPrice().replace("$", "");
+								 count+=Integer.parseInt(newPrice);
+							}							 
+							 System.out.println("Total = " + count + "$");
+							 
+								 
+							
+						//	 System.out.println("Total = " + count);
+							 
+							 System.out.println("Thank you for shopping keep your invoce number for replacement");
+							 state2=false;
+							 state=false;
+							
+						 }
+						 else if(itemChoice==5) {
+							 System.out.println("see you again");
+							 state2=false;
+							 state=false;
+						 }
+						 else {
+							System.out.println("choose numbers 1 thru 3");
+						}
+					}
+				}
+					
+						
+						
+						else if(choiceAgain==4) {
+							System.out.println("Please enter your invoice number");
+							Integer num=scanner.nextInt();
+							scanner.nextLine();
+							
+						}		
+						else if(choiceAgain==5) {
+							System.out.println("bye bye");
+							state=false;
+						}
+						
+		}
+				    
+				    else {
+				    	System.out.println("wrong password try again");
+				    }
+			}
+			
+			
+			
+			
+			
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}	
+		
+	}
+
+}
